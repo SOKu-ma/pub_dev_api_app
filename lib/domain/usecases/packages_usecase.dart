3e002e89d9ed6627d1cac686b2cc9e3f0f652699
otashuuhei <uverrevu812@gmail.com>
2025-09-21 11:53:23 +0900

    feat usecase impl 実装

--- lib/domain/usecases/packages_usecase.dart
+++ lib/domain/usecases/packages_usecase.dart
@@ -1,5 +1,17 @@
 import 'package:pub_dev_api_app/domain/entitis/packages_entity.dart';
+import 'package:pub_dev_api_app/domain/repositories/packages_repository.dart';
 
 abstract class PackagesUseCase {
   Future<List<PackagesEntity>> execute();
+}
+
+class PackagesUseCaseImpl implements PackagesUseCase {
+  final PackagesRepository _repository;
+
+  PackagesUseCaseImpl(this._repository);
+
+  @override
+  Future<List<PackagesEntity>> execute() {
+    return _repository.fetchPackagesList();
+  }
 }
